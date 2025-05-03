19d69aaddf5a9c1b18b0e96ef50e778e24afdbaf
ABenayas <benayas1987@gmail.com>
2025-05-03 13:26:46 +0200

    Se modifican los comentarios.

--- backend/src/movies/movies.module.ts
+++ backend/src/movies/movies.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
 
-@Module({ // Si generas este archivo por consola, te carga automáticamentes estos dos módulos. Y, como siempre, exportamos a app.module.ts.
+@Module({ // Si se genera este archivo por consola, carga automáticamentes estos dos módulos. Y, como siempre, exportamos a app.module.ts.
   controllers: [MoviesController],
   providers: [MoviesService]
 })
