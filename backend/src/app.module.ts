12f18e2d6ec29514a9f4428d5bd8635e244b2060
ABenayas <benayas1987@gmail.com>
2025-05-03 13:23:32 +0200

    Se generan las modificaciones necesarias para que corra correctamente.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,24 +4,28 @@ import { UsersModule } from './users/users.module';
 import { User } from './users/user.entity';
 import { AuthModule } from './auth/auth.module';
 import { MoviesModule } from './movies/movies.module';
+import { UserMoviesModule } from './user-movies/user-movies.module';
+import { Movie } from './movies/movie.entity';
+import { UserMovie } from './user-movies/user-movie.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost', // Tiene que coincidir con el servicio de docker-compose, elhost, si uso Docker, si no, localhost.
+      host: 'nestjs-database', // Tiene que coincidir con el servicio de docker-compose, el host si uso Docker, si no, localhost.
       port: 5432,
       username: 'postgres',
       password: 'postgres',
       database: 'default',
-      entities: [User], // Aquí metemos todas las entidades que tenemos. Aunque, parece que se puede hacer de manera automática así: 'dist/**/*.entity{.ts,.js}', si no, sería indicando las entidades una a una [User,...], e importándolas arriba.
-      synchronize: true, // Aquí está la opción de no sincronizar automáticamente, false. Al principio lo dejo true.
+      entities: [User, Movie, UserMovie], // Aquí metemos todas las entidades que tenemos. Aunque, parece que se puede hacer de manera automática así: 'dist/**/*.entity{.ts,.js}', si no, sería indicando las entidades una a una [User,...], e importándolas arriba.
+      synchronize: false, // Aquí está la opción de no sincronizar automáticamente, false. Al principio lo dejo true.
       retryDelay: 3000, // Estas 2 líneas son para darle tiempo a que la base de datos se caliente, estos son los segundos, y abajo los intentos de conexión.
       retryAttempts: 10,
     }),
     UsersModule, // Este es el módulo de usuarios.
     AuthModule, // Este es el módulo de auth.
     MoviesModule, // Este es el módulo de movies, parece que lo exportó solo al crear carpetas por consola.
+    UserMoviesModule,
   ], // Importamos los módulos que tengamos.
   controllers: [],
   providers: [],
